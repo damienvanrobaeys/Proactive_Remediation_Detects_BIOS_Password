a6e11e6964a3e2a5aef8017223de1f86d4b39806
Damien VAN ROBAEYS <damien.vanrobaeys@gmail.com>
2021-01-11 08:53:39 +0100

    Add files via upload

---
+++ Check_BIOS_Password_Detection.ps1
@@ -0,0 +1,27 @@
+$Get_Manufacturer_Info = (gwmi win32_computersystem).Manufacturer
+If($Get_Manufacturer_Info -like "*HP*")
+	{
+		$IsPasswordSet = Get-WmiObject -Namespace root/hp/instrumentedBIOS -Class HP_BIOSSettingInterface
+	} 
+ElseIf($Get_Manufacturer_Info -like "*Lenovo*")
+	{
+		$IsPasswordSet = (gwmi -Class Lenovo_BiosPasswordSettings -Namespace root\wmi -ComputerName $computer).PasswordState
+	} 
+ElseIf($Get_Manufacturer_Info -like "*Dell*")
+	{
+		$module_name = "DellBIOSProvider"
+		If (Get-Module -ListAvailable -Name $module_name){import-module $module_name -Force} 
+		Else{Install-Module -Name DellBIOSProvider -Force}	
+		$IsPasswordSet = (Get-Item -Path DellSmbios:\Security\IsAdminPasswordSet).currentvalue 	
+	} 
+
+If(($IsPasswordSet -eq 1) -or ($IsPasswordSet -eq $true))
+	{
+		write-output "Your BIOS is password protected"	
+		Exit 0
+	}
+Else
+	{
+		write-output "Your BIOS is not password protected"	
+		Exit 1
+	}
